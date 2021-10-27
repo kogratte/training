0a2462a38b5c8213b00b2ee646b253110099c333
kogratte <nicolas.issalene@gmail.com>
2021-10-27 11:28:46 +0200

    Optimise solution

--- src/index.js
+++ src/index.js
@@ -1,51 +1,77 @@
-module.exports = (blocks, reqs) => {
+const getBlockBuildings = (block) => Object.keys(block);
+
+/**
+ * 
+ * @param {*} blocks 
+ * @param {*} reqs 
+ * @returns 
+ */
+const computeBuildingPositions = (blocks, reqs) => {
 	const positions = {};
 
 	for (let i = 0; i < blocks.length; i++) {
-		Object.keys(blocks[i]).forEach(building => {
-
-			if (blocks[i][building]) {
-				positions[building] = positions[building] || [];
-				positions[building].push(i);
-			}
-		});
+		getBlockBuildings(blocks[i])
+			.filter(building => reqs.indexOf(building) !== -1 && blocks[i][building] === true)
+			.forEach(building => {
+					positions[building] = positions[building] || [];
+					positions[building].push(i);
+			});
 	}
 
-	const scoring = {};
+	return positions;
+}
+
+/**
+ * 
+ * @param {*} blocks 
+ * @param {*} reqs 
+ * @param {*} positions 
+ * @returns
+ * 
+ * Time complexity: O(blocks * blockBuildings * reqs) 
+ * Space complexity: O(blocks)
+ */
+const computeBlocksScorings = (blocks, reqs, positions) => {
+	const scoring = [];
 
 	for (let i = 0; i < blocks.length; i++) {
-		Object.keys(blocks[i]).filter(building => reqs.indexOf(building) !== -1).forEach(building => {
-			let distance = blocks.length + 1;
-				if (blocks[i][building]) {
-						distance = 0;
-				} else {
-					distance = Math.min(...positions[building].map(p => {
-						return Math.abs(i - p);
-					}));
-			}
+		const block = blocks[i];
+		scoring[i] = 0;
 
-			scoring[i] = {
-				...scoring[i] || { totalScore: 0 },
-				[building]: distance
-			};
+		getBlockBuildings(block)
+			.filter(building => reqs.indexOf(building) !== -1 && block[building] === false)
+			.forEach(building => {
+				const distance = Math.min(...positions[building].map(p => {
+					return Math.abs(i - p);
+				}));
 
-			scoring[i].totalScore += distance;
-		});
+				scoring[i] += distance;
+			});
 	}
 
-	const results = [];
+	return scoring;
+}
 
-	if (Object.keys(scoring).length) {
-		const totalScores = Object.keys(scoring).map(block => scoring[block].totalScore);
-		const bestScore = Math.min(...totalScores);
-		for (let i = 0; i < blocks.length; i++) {
-			if (scoring[i].totalScore === bestScore) {
-				results.push(i.toString());
-			}
-		}	
-	} else {
-		return[...Object.keys(blocks)];
-	}
 
-	return results;
+const buildResult = (scoring) => {
+	const bestScore = Math.min(...scoring);
+
+	const res = [];
+	scoring.reduce((acc, _, i) => {
+		if (_ === bestScore) {
+			acc.push(i.toString());
+		}
+		return acc;
+	}, res);
+
+	return res;
+}
+
+module.exports = (blocks, reqs) => {
+
+	const positions = computeBuildingPositions(blocks, reqs);
+	const scoring = computeBlocksScorings(blocks, reqs, positions);
+	const result = buildResult(scoring, blocks);
+
+	return result;
 };
